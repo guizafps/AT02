ec38e61add65dcbaaf204991f76457bcc7bd4108
Guilherme Bortolai <guilhermebortolai2004@gmail.com>
2023-08-07 20:02:45 -0300

    Métodos getter e setters implementados

--- at02.dart
+++ at02.dart
@@ -1,8 +1,14 @@
 class Teste {
-  String estudante;
-  double nota;
+  String _estudante;
+  double _nota;
 
-  Teste(this.estudante, this.nota);
+  Teste(this._estudante, this._nota);
+
+  String get estudante => _estudante;
+  set estudante(String novoEstudante) => _estudante = novoEstudante;
+
+  double get nota => _nota;
+  set nota(double novaNota) => _nota = novaNota;
 }
 
 void main() {
@@ -11,4 +17,9 @@ void main() {
 
   print('Estudante: ${teste1.estudante}, Nota: ${teste1.nota}');
   print('Estudante: ${teste2.estudante}, Nota: ${teste2.nota}');
+
+  teste1.estudante = 'Carlos';
+  teste1.nota = 7.0;
+
+  print('Estudante: ${teste1.estudante}, Nota: ${teste1.nota}');
 }
